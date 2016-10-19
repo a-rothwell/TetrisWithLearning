659e8b674e1f5d07e4a62cf9e87f9e2c2b3ea60d
Andrew Rothwell <a_rothwell@outlook.com>
2016-10-19 12:35:34 -0400

    Initial commit

---
+++ TetrisWithLearning.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class TetrisWithLearning{
+    private int[][] board = new int[24][10];
+    public TetrisWithLearning(){
+
+    }
+    public static void main(String[] args){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+    }
+}
